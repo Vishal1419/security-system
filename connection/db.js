976355c32987e4b058b21273bfad058c2b19c43e
Vishal Sherathiya <vishalsherathiya@gmail.com>
2021-01-10 14:02:08 +0530

    migrate to mongodb atlas from mlab

--- connection/db.js
+++ connection/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
 const config = require('../config');
 
-mongoose.connect(`mongodb://${config.ENV_CONFIG.db.host}:${config.ENV_CONFIG.db.port}/${config.ENV_CONFIG.db.database}`);
+mongoose.connect(config.ENV_CONFIG.db.connectionString);
